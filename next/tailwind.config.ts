1f2a2d25735bffb12f5535699638b0ddf7e44a34
SniperGER <6178765+SniperGER@users.noreply.github.com>
2024-12-04 10:02:25 +0100

    Add Next.js template

---
+++ next/tailwind.config.ts
@@ -0,0 +1,23 @@
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      screens: {
+        '3xl': '1720px',
+        '4xl': '2160px',
+      },
+    },
+  },
+  plugins: [typography],
+} satisfies Config;
